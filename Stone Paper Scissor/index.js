77f788ec9ea4712d0a54e4ea4f05372a106996e7
Solanki Bhavesh <78946856+developer-bhavesh@users.noreply.github.com>
2024-12-28 07:27:20 +0530

    updated

--- Stone Paper Scissor/index.js
+++ Stone Paper Scissor/index.js
@@ -1,2 +1,21 @@
 const username = document.getElementById("userName");
 username.textContent = prompt("Enter Your Name: ");
+const userImage = document.getElementById("userImage");
+
+function stonegame(){
+  let userChoice="stone"
+  game(userChoice)
+}
+function papergame(){
+  let userChoice="paper"
+  game(userChoice)
+}
+function scissorgame(){
+  let userChoice="scissor"
+  game(userChoice)
+}
+
+function game(userChoice){
+  console.log(`user choice is ${userChoice}`);
+  
+}
